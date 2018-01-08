aa522bb29714a8bbd57ff8ab8bc22a8e250c4679
Amey Malekar <admalekar@gmail.com>
2018-01-08 21:10:39 +0530

    clean up of code

--- app/src/main/java/emergencyapplication/com/emergencyapp/MainActivity.java
+++ app/src/main/java/emergencyapplication/com/emergencyapp/MainActivity.java
@@ -19,11 +19,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference root,root1,root2,root3,root4;
+    private DatabaseReference root, root1, root2, root3, root4;
 
     private RadioGroup rgroup;
     private RadioButton DtoA;
@@ -33,12 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton VtoB;
 
     private String latest_msg;
-    private String latest_msg_name;
+    private String station;
     private TextView Text_message;
 
     private Button send_alert;
-
-
 
 
     @Override
@@ -48,186 +48,38 @@ public class MainActivity extends AppCompatActivity {
 
         root = FirebaseDatabase.getInstance().getReference().getRoot().child("D_to_A");
         root1 = FirebaseDatabase.getInstance().getReference().getRoot().child("V_to_B");
-        root2 = FirebaseDatabase.getInstance().getReference().getRoot().child("E_to_M");
-        root3 = FirebaseDatabase.getInstance().getReference().getRoot().child("M_to_D");
+        root3 = FirebaseDatabase.getInstance().getReference().getRoot().child("E_to_M");
+        root2 = FirebaseDatabase.getInstance().getReference().getRoot().child("M_to_D");
         root4 = FirebaseDatabase.getInstance().getReference().getRoot().child("G_to_C");
-
 
 
         rgroup = findViewById(R.id.radioGroup);
         DtoA = findViewById(R.id.Channel_1);
+        VtoB = findViewById(R.id.Channel_2);
+        MtoD = findViewById(R.id.Channel_3);
         EtoM = findViewById(R.id.Channel_4);
         GtoC = findViewById(R.id.Channel_5);
-        MtoD = findViewById(R.id.Channel_3);
-        VtoB = findViewById(R.id.Channel_2);
+
+
         Text_message = findViewById(R.id.Scroll_text);
         send_alert = findViewById(R.id.Send_alert_button);
+
 
         send_alert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Send_alert_class.class);
+                Intent intent = new Intent(getApplicationContext(), Send_alert_class.class);
                 startActivity(intent);
             }
         });
-
-            root.limitToLast(10).addChildEventListener(new ChildEventListener() {
-
-                @Override
-                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                    Log.d("Added","DtoA");
-
-
-                    if(DtoA.isChecked()) {
-                        show_child(dataSnapshot);
-                    }
-
-                }
-
-                @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-
-
-
-        root1.limitToLast(10).addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
-                if(VtoB.isChecked()) {
-                    show_child(dataSnapshot);
-                }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-        root2.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                if(MtoD.isChecked()){
-                show_child(dataSnapshot);
-                }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-        root3.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-              if(EtoM.isChecked()) {
-                  show_child(dataSnapshot);
-              }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-        root4.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-               if(GtoC.isChecked()) {
-                   show_child(dataSnapshot);
-               }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
     }
+
+
 
     public void onCheckBox1(View view) {
 
-            Log.d("DtoA","true");
-        root.addChildEventListener(new ChildEventListener() {
+        Log.d("DtoA", "true");
+        root.limitToLast(10).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 show_child(dataSnapshot);
@@ -255,18 +107,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-
-
-
-
 
 
     public void onCheckBox2(View view) {
 
-        root1.addChildEventListener(new ChildEventListener() {
+        root1.limitToLast(10).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                System.out.println("checkbox2");
                 show_child(dataSnapshot);
             }
 
@@ -290,15 +138,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
 
 
         Log.d("cb2", "true2");
     }
+
     public void onCheckBox3(View view) {
 
         Log.d("cb3", "true3");
-        root2.addChildEventListener(new ChildEventListener() {
+        root2.limitToLast(10).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 show_child(dataSnapshot);
@@ -325,9 +173,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void onCheckBox4(View view) {
         Log.d("cb4", "true4");
-        root3.addChildEventListener(new ChildEventListener() {
+        root3.limitToLast(10).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 show_child(dataSnapshot);
@@ -355,11 +204,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
     public void onCheckBox5(View view) {
         Log.d("cb5", "true5");
-        root4.addChildEventListener(new ChildEventListener() {
+        root4.limitToLast(10).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                System.out.println("--");
                 show_child(dataSnapshot);
             }
 
@@ -388,19 +239,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void show_child(DataSnapshot dataSnapshot) {
-      Text_message.setText("");
-
+       // Text_message.setText("");
+        int count;
         Iterator i = dataSnapshot.getChildren().iterator();
+
         while (i.hasNext()) {
-
             latest_msg = (String) ((DataSnapshot) i.next()).getValue();
-
-            Text_message.append(latest_msg + "\n\n");
-
+            station = (String) ((DataSnapshot) i.next()).getValue();
+            Text_message.append(station + " :" + latest_msg + "\n\n");
 
         }
     }
-
-
-
 }
--- app/src/main/java/emergencyapplication/com/emergencyapp/Send_alert_class.java
+++ app/src/main/java/emergencyapplication/com/emergencyapp/Send_alert_class.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -23,18 +24,21 @@ import java.util.Map;
  * Created by Amey on 14-Nov-17.
  */
 
-public class Send_alert_class extends AppCompatActivity{
+public class Send_alert_class extends AppCompatActivity {
 
 
     private DatabaseReference rootAlert;
     private Button send_button_2;
-    private String user = "Amey";
+
     private String tempKey;
     private EditText Text_to_send;
-  //  public ArrayList<String> station_name;
     private Spinner station_spinner;
+    private Spinner emergency_service_spinner;
     public ArrayList<String> stations;
+    public ArrayList<String> emergency_spinner;
+
     private String selected_channel;
+    private String sendText;
 
 
     @Override
@@ -42,11 +46,17 @@ public class Send_alert_class extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_alert_layout);
         station_spinner = findViewById(R.id.station_spinner);
+        emergency_service_spinner = findViewById(R.id.emer_spinner);
 
         send_button_2 = findViewById(R.id.Send_button_2);
         Text_to_send = findViewById(R.id.Send_text);
 
-       stations = new ArrayList<>();
+        stations = new ArrayList<>();
+        emergency_spinner = new ArrayList<>();
+        emergency_spinner.add("Fire Station");
+        emergency_spinner.add("Ambulance");
+        emergency_spinner.add("Police Station");
+
         stations.add("Dahisar");
         stations.add("Borivali");
         stations.add("Kandivili");
@@ -55,7 +65,7 @@ public class Send_alert_class extends AppCompatActivity{
         stations.add("Jogeshwari");
         stations.add("Andheri");
         stations.add("Vile Parle");
-        stations.add("Santacruz");
+        stations.add("Santa Cruz");
         stations.add("Khar Road");
         stations.add("Bandra");
         stations.add("Mahim");
@@ -70,42 +80,36 @@ public class Send_alert_class extends AppCompatActivity{
         stations.add("Marine Lines");
         stations.add("Churchgate ");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,stations);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, stations);
         station_spinner.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, emergency_spinner);
+        emergency_service_spinner.setAdapter(adapter2);
 
 
         station_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (station_spinner.getSelectedItem().equals("Dahisar")|| station_spinner.getSelectedItem().equals("Borivali")||station_spinner.getSelectedItem().equals("Kandivali")||station_spinner.getSelectedItem().equals("Malad")||station_spinner.getSelectedItem().equals("Goregoan")||station_spinner.getSelectedItem().equals("Jogeshwari")||station_spinner.getSelectedItem().equals("Andheri"))
-                {
+                if (station_spinner.getSelectedItem().equals("Dahisar") || station_spinner.getSelectedItem().equals("Borivali") || station_spinner.getSelectedItem().equals("Kandivali") || station_spinner.getSelectedItem().equals("Malad") || station_spinner.getSelectedItem().equals("Goregaon") || station_spinner.getSelectedItem().equals("Jogeshwari") || station_spinner.getSelectedItem().equals("Andheri")) {
                     selected_channel = "D_to_A";
-                    Log.d("hsdfn",selected_channel);
+                    Log.d("hsdfn", selected_channel);
                     rootAlert = FirebaseDatabase.getInstance().getReference().child(selected_channel);
-                }
-                else if (station_spinner.getSelectedItem().equals("Vile Parle")|| station_spinner.getSelectedItem().equals("Santa Cruz")||station_spinner.getSelectedItem().equals("Khar Road")||station_spinner.getSelectedItem().equals("Bandra"))
-                {
+                } else if (station_spinner.getSelectedItem().equals("Vile Parle") || station_spinner.getSelectedItem().equals("Santa Cruz") || station_spinner.getSelectedItem().equals("Khar Road") || station_spinner.getSelectedItem().equals("Bandra")) {
                     selected_channel = "V_to_B";
-                    Log.d("hsdfn",selected_channel);
+                    Log.d("hsdfn", selected_channel);
                     rootAlert = FirebaseDatabase.getInstance().getReference().child(selected_channel);
-                }
-
-                else if (station_spinner.getSelectedItem().equals("Mahim")|| station_spinner.getSelectedItem().equals("Matunga")||station_spinner.getSelectedItem().equals("Dadar"))
-                {
+                } else if (station_spinner.getSelectedItem().equals("Mahim") || station_spinner.getSelectedItem().equals("Matunga") || station_spinner.getSelectedItem().equals("Dadar")) {
                     selected_channel = "M_to_D";
-                    Log.d("hsdfn",selected_channel);
+                    Log.d("hsdfn", selected_channel);
                     rootAlert = FirebaseDatabase.getInstance().getReference().child(selected_channel);
-                }
-                else if (station_spinner.getSelectedItem().equals("Elphinstone Road")|| station_spinner.getSelectedItem().equals("Lower Parel")||station_spinner.getSelectedItem().equals("Maha Laxmi")||station_spinner.getSelectedItem().equals("Mumbai Central"))
-                {
+                } else if (station_spinner.getSelectedItem().equals("Elphinstone Road") || station_spinner.getSelectedItem().equals("Lower Parel") || station_spinner.getSelectedItem().equals("Maha Laxmi") || station_spinner.getSelectedItem().equals("Mumbai Central")) {
                     selected_channel = "E_to_M";
-                    Log.d("hsdfn",selected_channel);
+                    Log.d("hsdfn", selected_channel);
                     rootAlert = FirebaseDatabase.getInstance().getReference().child(selected_channel);
-                }
-                else
+                } else
 
                     selected_channel = "G_to_C";
-                Log.d("hsdfn",selected_channel);
+                Log.d("hsdfn", selected_channel);
                 rootAlert = FirebaseDatabase.getInstance().getReference().child(selected_channel);
 
             }
@@ -116,41 +120,27 @@ public class Send_alert_class extends AppCompatActivity{
             }
         });
 
-
-
-
-
-
-
-        //station_spinner.setAdapter(new ArrayAdapter<String>(Send_alert_class.class, android.R.layout.simple_spinner_dropdown_item, stations ));
-
-
         send_button_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Map<String,Object> map = new HashMap<String, Object>() ;
+                Map<String, Object> map = new HashMap<String, Object>();
 
                 tempKey = rootAlert.push().getKey();
                 rootAlert.updateChildren(map);
 
                 DatabaseReference message_root = rootAlert.child(tempKey);
                 Map<String, Object> map2 = new HashMap<String, Object>();
-               // map2.put("name", user);
+                map2.put("name", station_spinner.getSelectedItem());
                 map2.put("msg", Text_to_send.getText().toString());
-
 
 
                 message_root.updateChildren(map2);
 
+                sendText = "Alert is created and " + emergency_service_spinner.getSelectedItem() + " is called to " + station_spinner.getSelectedItem();
 
+                Toast.makeText(getApplicationContext(), sendText, Toast.LENGTH_SHORT).show();
 
             }
         });
-
-
     }
-
-
-
-
 }
